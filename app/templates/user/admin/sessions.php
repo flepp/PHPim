4594463c32a1f3546712b0ba3b65eac4516354cf
totossj4 <ibrahima.thiello@hotmail.fr>
2016-07-12 11:08:55 +0200

    modification de route User

--- app/templates/user/admin/sessions.php
+++ app/templates/user/admin/sessions.php
@@ -1,6 +1,15 @@
 <?php $this->layout('layout', ['title' => 'Sessions']) ?>
 
 <?php $this->start('main_content') ?>
-	<p>Sessions</p>
+	<p>Créer une session </p>
+	<form method="POST" action="">
+		<fieldset>
+			<legend>Ajouter une nouvelle session</legend>
+			<label>Nom de la session:</label>
+			<input type="text" name="sessionName">
+			<label>Date de début:</label>
+			<input type="" name="">
+		</fieldset>
+	</form>
 <?php $this->stop('main_content') ?>
 
