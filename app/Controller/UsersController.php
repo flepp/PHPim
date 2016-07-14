7cebf2a98e14220f1045100a1ef7753fbcb67de9
totossj4 <ibrahima.thiello@hotmail.fr>
2016-07-14 14:12:30 +0200

    upload OK

--- app/Controller/UsersController.php
+++ app/Controller/UsersController.php
@@ -62,6 +62,48 @@ class UsersController extends Controller
         
         $this->show('user/edit');
     }
+    public function invitations(){
+        $this->show('user/admin/invitations');
+    }
 
+    /*-----Uploading Users list form a file and sending them an invintation to register-----*/
+    public function invitationsPost(){
+        if (!empty($_POST)) {
+        debug($_POST);
+        debug($_FILES);
+
+        $extensionAutorisees = array('txt');
+
+        // Je récupère mon tableau avec les infos sur le fichier
+        foreach ($_FILES as $key => $fichier) {
+            // Je teste si le fichier a été uploadé
+            if (!empty($fichier) && !empty($fichier['name'])) {
+                print_r($fichier);
+                if ($fichier['size'] <= 250000) {
+                    $filename = $fichier['name'];
+                    $dotPos = strrpos($filename, '.');
+                    $extension = strtolower(substr($filename, $dotPos+1));
+                    // Je test si c'est pas un hack (sur l'extension)
+                    //if (substr($fichier['name'], -4) != '.php') {
+                    if (in_array($extension, $extensionAutorisees)) {
+                        // Je déplace le fichier uploadé au bon endroit
+                        if (move_uploaded_file($fichier['tmp_name'], 'C:\xampp\htdocs\PHPim\public\assets\upload\text/'.$filename)) {
+                            echo 'fichier téléversé<br />';
+                        }
+                        else {
+                            echo 'une erreur est survenue<br />';
+                        }
+                    }
+                    else {
+                        echo 'extension interdite<br />';
+                    }
+                }
+                else {
+                    echo 'fichier trop lourd<br />';
+                }
+            }
+    }
 }
- ?>
+    }
+}
+?>
