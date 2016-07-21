07fb988b45403af41be44fc878e6e56905456b40
pageioa <paul.ioanid@yahoo.com>
2016-07-21 14:48:21 +0200

    Solved the display of students according the session in which they had been trained.

--- app/Manager/AllUsersManager.php
+++ app/Manager/AllUsersManager.php
@@ -2,11 +2,22 @@
 
 namespace Manager;
 
-class AllUsersManager extends \W\Manager\UserManager
-{
+class AllUsersManager extends \W\Manager\UserManager{
 	public function __construct() {
 		parent::__construct();
 		// I'm defining the table name
 		$this->setTable('users');
+	}
+
+	public function findAllUsersFromSession($id){
+		if (!is_numeric($id)){
+			return false;
+		}
+		$sql = 'SELECT * FROM '.$this->table.' WHERE session_id = :id';
+		$sth = $this->dbh->prepare($sql);
+		$sth->bindValue(':id', $id);
+		$sth->execute();
+
+		return $sth->fetchAll();
 	}
 }
