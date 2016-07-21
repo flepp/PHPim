e61a206b2a4b15f2fc4ba3e95a093ff0ee5d469b
totossj4 <ibrahima.thiello@hotmail.fr>
2016-07-21 22:26:58 +0200

    -Plus de style dans mon html -Plus de <br/> dans mon html -Indentation OK pour mon html -Indentation nav layout

--- app/templates/user/admin/invitations.php
+++ app/templates/user/admin/invitations.php
@@ -1,72 +1,52 @@
 <?php $this->layout('layout', ['title' => 'Invitations']) ?>
 
 <?php $this->start('main_content') ?>
-	<br/>
-	<br/>
-	<br/>
-	<h1>Ajouter une liste d'étudiant</h1>
-	<br/>
-	
-	<br/>
-	<form action="" method="post" enctype="multipart/form-data">
-		<input type="hidden" name="fichierSoumis" value="1">
-		<input type="file" name="fichierteleverse">
-		<br/>
-		<p>*extension permise: .csv / Séparateur ',' ou ';'</p>
-		<p>Ordre: Prénom/Nom/Email/Pseudo</p>
-		<br/>
-		<button type="submit" name="upload">Téléverser</button>
-	</form>
-	<br/>
-	<br/>
-	<br/>
-	<br/>
-	<br/>
-	<form method="POST" action="">
-		<p>Choisissez une session</p>
-		<br> 
-		<select name="session">
-		<?php foreach ($sessionList as $key => $value): ?>
-			<option value="<?= $value['id'] ?>"><?= $value['ses_name'] ?></option>
+
+<!-- ~~~~~~~~~~~~~~~~~~~~~~~~~~~UPLOADING STUDENT LIST~~~~~~~~~~~~~~~~~~~~~ -->
+<h1>Ajouter une liste d'étudiant</h1>
+<form action="" method="post" enctype="multipart/form-data">
+	<input type="hidden" name="fichierSoumis" value="1">
+	<input type="file" name="fichierteleverse">
+	<span>*extension permise: .csv / Séparateur ',' ou ';'</span>
+	<span>Ordre: Prénom/Nom/Email/Pseudo</span>
+	<button type="submit" name="upload">Téléverser</button>
+</form>
+
+<!-- ~~~~~~~~~~~~~~~~~~~~~~~~~~~SENDING INVITATIONS TO STUDENTS~~~~~~~~~~~~~~~~~~~~~ -->
+<h1>Choisissez une session</h1> 
+<form method="POST" action="">
+	<select name="session">
+	<?php foreach ($sessionList as $key => $value): ?>
+		<option value="<?= $value['id'] ?>"><?= $value['ses_name'] ?></option>
+	<?php endforeach ?>
+	</select> 
+	<?php if (isset($arrayStudents) && sizeof($arrayStudents) > 0): ?>
+		<?php foreach ($arrayStudents as $key => $students): ?>
+			<div>
+				<label>#<?= $key.' ' ?>Prénom:</label> 
+				<input type="text" name="student[<?= $key ?>][firstname]" value="<?=$students['0'] ?>">
+				<label>Nom:</label>
+				<input type="text" name="student[<?= $key ?>][name]" value="<?=$students['1'] ?>">
+				<label>Pseudo:</label>
+				<input type="text" name="student[<?= $key ?>][pseudo]" value="<?=$students['3'] ?>">
+				<label>Email:</label>
+				<input type="email" name="student[<?= $key ?>][email]" value="<?=$students['2'] ?>">
+			</div>
 		<?php endforeach ?>
-		</select>
-		<br> 
-		<br>
-		<br>
-		<br>
-		<?php if (isset($arrayStudents) && sizeof($arrayStudents) > 0): ?>
-			<?php foreach ($arrayStudents as $key => $students): ?>
-				<div>
-					<label>#<?= $key.' ' ?>Prénom:</label> 
-					<input type="text" name="student[<?= $key ?>][firstname]" value="<?=$students['0'] ?>">
-					<label>Nom:</label>
-					<input type="text" name="student[<?= $key ?>][name]" value="<?=$students['1'] ?>">
-					<label>Pseudo:</label>
-					<input type="text" name="student[<?= $key ?>][pseudo]" value="<?=$students['3'] ?>">
-					<label>Email:</label>
-					<input type="email" name="student[<?= $key ?>][email]" value="<?=$students['2'] ?>">
-				</div>
-			<?php endforeach ?>
-		<?php endif ?>
-		<br>
-		<button type="submit" name="sendInvitations">Envoyez les invitations!</button>
-	</form>
-	<br> 
-	<br> 
-	<br> 
-	<p>Vous êtes connecté en tant que:</p>
-	<br> 
-	<br>
-	<form method="post" action="">
-		<select name="userInfo">
-			<?php foreach ($usersList as $key => $info): ?>
-				<option value="<?= $info['id'] ?>" "<?= $info['id'] == $_SESSION['user']['id'] ? ' selected="selected"' : '' ?>"><?= $info['usr_name'].' ' ?><?= $info['usr_firstname'] ?></option>
-			<?php endforeach ?>
-		</select>
-		<button type="sumit" name="troll">Troll Me</button>
-	</form>
-	<br> 
-	<br>
+	<?php endif ?>
+	<button type="submit" name="sendInvitations">Envoyez les invitations!</button>
+</form>
+
+<!-- ~~~~~~~~~~~~~~~~~~~~~~~~~~~CHANGING USER PROFILE FOR ADMIN~~~~~~~~~~~~~~~~~~~~~ -->   
+<h1>Vous êtes connecté en tant que:</h1> 
+<form method="post" action="">
+	<select name="userInfo">
+		<?php foreach ($usersList as $key => $info): ?>
+			<option value="<?= $info['id'] ?>" "<?= $info['id'] == $_SESSION['user']['id'] ? ' selected="selected"' : '' ?>"><?= $info['usr_name'].' ' ?><?= $info['usr_firstname'] ?></option>
+		<?php endforeach ?>
+	</select>
+	<button type="sumit" name="troll">Troll Me</button>
+</form> 
 
 <?php $this->stop('main_content') ?>
 
--- app/templates/user/admin/sessions.php
+++ app/templates/user/admin/sessions.php
@@ -1,53 +1,47 @@
 <?php $this->layout('layout', ['title' => 'Sessions']) ?>
 
 <?php $this->start('main_content') ?>
-<br/>
-<br/>
-<br/>
-<br/>
+
 <h1>Créer une session </h1>
-<br/>
-<br/>
-<br/>
+
+<!-- ~~~~~~~~~~~~~~~~~~~~~~~~~~~SESSION CREATION SECTION~~~~~~~~~~~~~~~~~~~~~ -->
 <section>
 	<form method="POST" action="">
 		<fieldset>
-			<legend>Ajouter une nouvelle session</legend><br/><br/>
-			<label>Nom de la session:</label><br/>
-			<input type="text" name="sessionName"><br/><br/>
-			<label>Date de début:</label><br/>
-			<input type="date" name="sessionStart"><br/><br/>
-			<label>Date de fin:</label><br/>
-			<input type="date" name="sessionEnd"><br/><br/>
+			<legend>Ajouter une nouvelle session</legend>
+			<label>Nom de la session:</label>
+			<input type="text" name="sessionName">
+			<label>Date de début:</label>
+			<input type="date" name="sessionStart">
+			<label>Date de fin:</label>
+			<input type="date" name="sessionEnd">
 			<button type="submit" name="sessionCreate">Créer</button>
 		</fieldset>
 	</form>
-	<br/>
-	<br/>
-	<br/>
-	<h2>Liste des sessions:</h2>
-	<br/>
-	<br/>
-	<br/>
-	<br/>
+</section>
+
+<!-- ~~~~~~~~~~~~~~~~~~~~~~~~~~~SESSION EDITION SECTION~~~~~~~~~~~~~~~~~~~~~ -->
+<section>
+	<h1>Liste des sessions:</h1>
 	<ul>
 		<?php foreach($sessionList as $key => $value) : ?>
-			<li style="display:inline"><?= $value['ses_name'] ?> du <?= $value['ses_start'] ?> au <?= $value['ses_end'] ?> status(<?= $value['ses_status'] ?>)</li> 
-			<div style="display:inline">
-				<form method="POST" action="" style="display:inline">
+			<li><?= $value['ses_name'] ?> du <?= $value['ses_start'] ?> au <?= $value['ses_end'] ?> status(<?= $value['ses_status'] ?>)</li> 
+			<div>
+				<form method="POST" action="">
 					<input hidden type="text" name="sessionId" value="<?= $value['id'] ?>">
 					<input hidden type="text" name="sessionStatus" value="1">
 					<button type="submit" name="sessionOn">ON</button>
 				</form>
-				<form method="POST" action="" style="display:inline">
+				<form method="POST" action="">
 					<input hidden type="text" name="sessionId" value="<?= $value['id'] ?>">
 					<input hidden type="text" name="sessionStatus" value="0">
 					<button type="submit" name="sessionOff">OFF</button>
 				</form>
 			</div>
-			<form method="POST" action="" style="display:inline">
+			<form method="POST" action="">
 				<input hidden type="text" name="sessionName" value="<?= $value['ses_name'] ?>">
 				<input hidden type="text" name="sessionId" value="<?= $value['id'] ?>">
+				<button type="button">Editer</button>
 				<button type="submit" name="sessionDelete">Supprimer</button>
 			</form>
 			<form method="POST" action="">
@@ -58,10 +52,8 @@
 				<input type="date" name="sessionEnd" value="<?= $value['ses_end'] ?>">
 				<button type="submit" name="sessionEdit">Modifier</button>
 			</form>
-			<br>
-			<br>
-			<br>
 		<?php endforeach; ?>
 	</ul>
 </section>
+
 <?php $this->stop('main_content') ?>
