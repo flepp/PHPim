345582a129aae3d4771e08ad2e4f257fce0e66f5
flepp <flepptasch@hotmail.com>
2016-07-27 00:37:40 +0200

    homepage done nav almost done

--- app/templates/layout.php
+++ app/templates/layout.php
@@ -18,7 +18,7 @@
 	<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.1.0/jquery.min.js"></script>
 </head>
 <body>
-	<header class="header-navigation">
+	<header>
 		<nav class="navbar">
 			<div class="container">
 				<!-- Brand and toggle get grouped for better mobile display -->
@@ -29,7 +29,7 @@
 					<span class="icon-bar"></span>
 					<span class="icon-bar"></span>
 					</button>
-					<a class="navbar-brand" href="<?= $this->url('default_home') ?>">PHPim</a>
+					<a class="navbar-brand" href="<?= $this->url('default_home') ?>"><span>PHP</span><span>im</span></a>
 				</div>
 				<!-- Collect the nav links, forms, and other content for toggling -->
 				<div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
@@ -41,10 +41,10 @@
 						<li class="dropdown">
 							<a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Administration <span class="caret"></span></a>
 							<ul class="dropdown-menu">
-								<li><a href="<?= $this->url('session_session') ?>">Sessions</a></li>
-								<li><a href="<?= $this->url('user_invitations') ?>">Invitations</a></li>
-								<li><a href="<?= $this->url('quiz_manage') ?>">Gestion Quiz</a></li>
-								<li><a href="<?= $this->url('session_database') ?>">BDD Session</a></li>
+								<li class="col-sm-4"><a href="<?= $this->url('session_session') ?>">Sessions</a></li>
+								<li class="col-sm-4"><a href="<?= $this->url('user_invitations') ?>">Invitations</a></li>
+								<li class="col-sm-4"><a href="<?= $this->url('quiz_manage') ?>">Gestion Quiz</a></li>
+								<li class="col-sm-4"><a href="<?= $this->url('session_database') ?>">BDD Session</a></li>
 							</ul>
 						</li>
 						<?php endif ?>
@@ -53,8 +53,13 @@
 						<li><a href="<?= $this->url('user_database') ?>">BDD utilisateur</a></li>
 						<li><a href="<?= $this->url('allusers_allUsers') ?>">Etudiants</a></li>
 						<li class="dropdown">
-							<a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Profil <span class="caret"></span></a>
-							<ul class="dropdown-menu">
+							<a href="#" class="dropdown-toggle profile-img" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">
+								<span class="profile-text">Profil</span>
+								<svg version="1.1" id="Capa_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px" width="512px" height="512px" viewBox="0 0 512 512" enable-background="new 0 0 512 512" xml:space="preserve">
+									<path d="M128,160c0,70.692,57.308,128,128,128s128-57.308,128-128S326.692,32,256,32S128,89.308,128,160z M384,320H128  C57.308,320,0,377.309,0,448v32h4.033h503.934H512v-32C512,377.309,454.692,320,384,320z"/>
+								</svg>
+							</a>
+							<ul class="dropdown-menu nav-sub-menu">
 								<li><a href="<?= $this->url('allusers_details', ['id' => $_SESSION['user']['id']]) ?>">Mon Profil</a></li>
 								<li><a href="<?php echo $this->url('user_logout'); ?>">Déconnexion</a></li>
 							</ul>
