aa5a3c1093625cc65adbc1d15f4792735f338278
totossj4 <ibrahima.thiello@hotmail.fr>
2016-07-26 22:14:32 +0200

    disposition de la page session OK, full responsive . Il ne reste plus que les couleurs!

--- app/templates/user/admin/sessions.php
+++ app/templates/user/admin/sessions.php
@@ -1,94 +1,106 @@
 <?php $this->layout('layout', ['title' => 'Sessions']) ?>
 
 <?php $this->start('main_content') ?>
-<div class="container">
+
+<div class="container container-i">
 	<!-- ~~~~~~~~~~~~~~~~~~~~~~~~~~~SESSION CREATION SECTION~~~~~~~~~~~~~~~~~~~~~ -->
-	<section class="row firstSection">
-		<h1 class="col-xs-12 col-sm-6">Créer une session </h1>
-			<div class="row">
-				<form method="POST" action="" class="col-xs-10 col-xs-offset-1 col-sm-6">
-					<div class="form-group">
-						<label>Nom de la session:</label>
-						<input type="text" name="sessionName" class="form-control">
-					</div>
-					<div class="form-group">
-						<label>Date de début:</label>
-						<input type="date" name="sessionStart" class="form-control">
-					</div>
-					<div class="form-group">
-						<label>Date de fin:</label>
-						<input type="date" name="sessionEnd" class="form-control">
-					</div>
-						<button type="submit" name="sessionCreate" class="btn btn-default">Ajouter</button>
-				</form>
-			</div>
+	<section class="row col-md-6">
+		<div class="col-xs-8 col-xs-offset-2 col-sm-6">
+			<h1 class="">Créer une session </h1>
+			<form method="POST" action="">
+				<div class="form-group">
+					<label>Nom de la session:</label>
+					<input type="text" name="sessionName" class="form-control">
+				</div>
+				<div class="form-group">
+					<label>Date de début:</label>
+					<input type="date" name="sessionStart" class="form-control">
+				</div>
+				<div class="form-group">
+					<label>Date de fin:</label>
+					<input type="date" name="sessionEnd" class="form-control">
+				</div>
+				<button type="submit" name="sessionCreate" class="btn btn-default center-block">Créer</button>
+			</form>
+		</div>
 	</section>
 
 	<!-- ~~~~~~~~~~~~~~~~~~~~~~~~~~~SESSION EDITION SECTION~~~~~~~~~~~~~~~~~~~~~ -->
-	<section class="row secondSection">
-
-			<h1 class="col-xs-12 col-sm-6">Liste des sessions</h1>
+	<section class="col-md-6">
+		<div class="col-xs-8 col-xs-offset-2 col-sm-6">
+			<h1 class="">Sessions créees</h1>
 			<!---------------SESSION WITH STUDENTS----------- -->
-			<ul  class="col-xs-12 col-sm-6 col-sm-offset-1">
+			<h2>Sessions remplies</h2>
+			<ul>
 				<?php foreach($sessionList as $key => $value) : ?>
-					<div class="row"> 
-						<li class="col-xs-9"><?= $value['ses_name'] ?> du <?= $value['ses_start'] ?> / <?= $value['ses_end'] ?></li>
+					<li class="text-capitalize"><strong><?= $value['ses_name'] ?> du <?= $value['ses_start'] ?> au <?= $value['ses_end'] ?></strong></li>
+					<div class="row">
 						<div class="col-xs-3">
-							<div class="row">
-								<form method="POST" action="" class="col-xs-5">
-									<input hidden type="text" name="sessionId" value="<?= $value['id'] ?>">
-									<input hidden type="text" name="sessionStatus" value="1">
-									<button type="submit" name="sessionOn" class="enableSession btn btn-default">1</button>
-								</form>
-								<form method="POST" action="" class="col-xs-5">
-									<input hidden type="text" name="sessionId" value="<?= $value['id'] ?>">
-									<input hidden type="text" name="sessionStatus" value="0">
-									<button type="submit" name="sessionOff" class="disableSession btn btn-default">0</button>
-								</form>
-							</div>
 							<button type="button" class="button_edit btn btn-default">Editer</button>
 						</div>
+						<div class="form-group col-xs-4 col-xs-offset-1">
+							<form method="POST" action="" <?php if($value['ses_status'] == 1){echo " hidden";} ?>>
+								<input hidden type="text" name="sessionId" value="<?= $value['id'] ?>">
+								<input hidden type="text" name="sessionStatus" value="1">
+								<button type="submit" name="sessionOn" class="enableSession btn btn-default">Réactivé</button>
+							</form>
+							<form method="POST" action="" <?php if($value['ses_status'] == 0){echo " hidden";} ?>>
+								<input hidden type="text" name="sessionId" value="<?= $value['id'] ?>">
+								<input hidden type="text" name="sessionStatus" value="0">
+								<button type="submit" name="sessionOff" class="disableSession btn btn-default">Désactivé</button>
+							</form>
+						</div>
 					</div>
-					<div class="row">
-						<form hidden method="POST" action="" class="show_edit col-xs-10 col-xs-offset-1">
-							<input hidden type="text" name="sessionName" value="<?= $value['ses_name'] ?>">
-							<input hidden type="text" name="sessionId" value="<?= $value['id'] ?>">
-							<input class="form-control" type="text" name="sessionName" value="<?= $value['ses_name'] ?>">
-							<input class="form-control" type="date" name="sessionStart" value="<?= $value['ses_start'] ?>">
-							<input class="form-control" type="date" name="sessionEnd" value="<?= $value['ses_end'] ?>">
-							<button type="submit" name="sessionEdit" class="update btn btn-default">Modifier</button>
-						</form>
-					</div>
+					<form hidden method="POST" action="" class="show_edit">
+						<input hidden type="text" name="sessionName" value="<?= $value['ses_name'] ?>">
+						<input hidden type="text" name="sessionId" value="<?= $value['id'] ?>">
+						<div class="form-group">
+							<input type="text" name="sessionName" value="<?= $value['ses_name'] ?>" class="form-control">
+						</div>
+						<div class="form-group">
+							<input type="date" name="sessionStart" value="<?= $value['ses_start'] ?>" class="form-control">
+						</div>
+						<div class="form-group">
+							<input type="date" name="sessionEnd" value="<?= $value['ses_end'] ?>" class="form-control">
+						</div>
+						<button type="submit" name="sessionEdit" class="update btn btn-default">Modifier</button>
+					</form>
 				<?php endforeach; ?>
 			</ul>
-		<!---------------SESSION WITH NO STUDENTS----------- -->
-		<ul class="col-xs-12">
-			<?php foreach($sessionList2 as $key => $value) : ?>
-				<div class="row">
-					<li class="col-xs-9"><?= $value['ses_name'] ?> du <?= $value['ses_start'] ?> / <?= $value['ses_end'] ?></li> 
-					<div class="col-xs-3">
-						<form method="POST" action="" class="">
-							<input hidden type="text" name="sessionName" value="<?= $value['ses_name'] ?>">
-							<input hidden type="text" name="sessionId" value="<?= $value['id'] ?>">
-							<button type="submit" name="sessionDelete" class="delete btn btn-default">Effacer</button>
-						</form>
-						<div>
+			<!---------------SESSION WITH NO STUDENTS----------- -->
+			<h2>Sessions vides</h2>
+			<ul>
+				<?php foreach($sessionList2 as $key => $value) : ?>
+					<li class="text-capitalize"><strong><?= $value['ses_name'] ?> du <?= $value['ses_start'] ?> au <?= $value['ses_end'] ?></strong></li>
+					<div class="row">
+						<div class="col-xs-3">
 							<button type="button" class="button_edit btn btn-default">Editer</button>
 						</div>
+						<div class="col-xs-4 col-xs-offset-1">
+							<form method="POST" action="">
+								<input hidden type="text" name="sessionName" value="<?= $value['ses_name'] ?>">
+								<input hidden type="text" name="sessionId" value="<?= $value['id'] ?>">
+								<button type="submit" name="sessionDelete" class="delete btn btn-default">Supprimer</button>
+							</form>
+						</div>
 					</div>
-					<div class="row">
-						<form hidden method="POST" action="" class="show_edit col-xs-10 col-xs-offset-1">
-							<input hidden type="text" name="sessionName" value="<?= $value['ses_name'] ?>">
-							<input hidden type="text" name="sessionId" value="<?= $value['id'] ?>">
-							<input class="form-control" type="text" name="sessionName" value="<?= $value['ses_name'] ?>">
-							<input class="form-control" type="date" name="sessionStart" value="<?= $value['ses_start'] ?>">
-							<input class="form-control" type="date" name="sessionEnd" value="<?= $value['ses_end'] ?>">
-							<button type="submit" name="sessionEdit" class="update btn btn-default">Modifier</button>
-						</form>
-					</div>
-				</div>
-			<?php endforeach; ?>
-		</ul>
+					<form hidden method="POST" action="" class="show_edit">
+						<input hidden type="text" name="sessionName" value="<?= $value['ses_name'] ?>">
+						<input hidden type="text" name="sessionId" value="<?= $value['id'] ?>">
+						<div class="form-group">
+							<input type="text" name="sessionName" value="<?= $value['ses_name'] ?>" class="form-control">
+						</div>
+						<div class="form-group">
+							<input type="date" name="sessionStart" value="<?= $value['ses_start'] ?>" class="form-control">
+						</div>
+						<div class="form-group">
+							<input type="date" name="sessionEnd" value="<?= $value['ses_end'] ?>" class="form-control">
+						</div>
+						<button type="submit" name="sessionEdit" class="update btn btn-default">Modifier</button>
+					</form>
+				<?php endforeach; ?>
+			</ul>
+		<div class="col-xs-8 col-xs-offset-2">
 	</section>
 </div>
 <?php $this->stop('main_content') ?>
