999cf4755acecf7a0008b0294264efc3bca97ce6
Michel Cavro <mcavro@pt.lu>
2016-07-26 11:48:25 +0200

    WIP register

--- app/templates/default/about.php
+++ app/templates/default/about.php
@@ -3,34 +3,53 @@
 		<?php $this->start('main_content') ?>
 			<h2>Ce projet à été réalisé par l'équipe de WEBFORCEONE de la session 2 (du 04-04-2016 au 29-07-2016).</h2>
 
-		<div class="flip-container" ontouchstart="this.classList.toggle('hover');">Ibrahima
+		<div class="flip-container" ontouchstart="this.classList.toggle('hover');">
 			<div class="flipper">
 				<div class="front">
 					<img height="297px" width="250px" src="<?=$this->assetUrl('upload/img/Ibrahima-THIELO.jpg')?>">
 				</div>
 				<div class="back">
-					nom: Ibrahima Thiello
-					linkedin:ibrahima-thiello
-					twitter:ibrah_thiello
-					@: ibrahima.thiello@hotmail.fr
+					nom: Ibrahima Thiello <br>
+					linkedin:ibrahima-thiello <br>
+					twitter:ibrah_thiello <br>
+					@: ibrahima.thiello@hotmail.fr <br>
 					<img height="38px" width="60px" src="<?=$this->assetUrl('upload/img/flag_sen.png')?>">
 				</div>
 			</div>
 		</div>
+		<div class="flip-container" ontouchstart="this.classList.toggle('hover');">
+			<div class="flipper">
+				<div class="front">
+					<img height="297px" width="250px" src="<?=$this->assetUrl('upload/img/Michel-CAVRO.jpg')?>">
+				</div>
+				<div class="back">
+					nom: Michel Cavro <br>
+					linkedin.com/in/michelcavro <br>
+					<img height="38px" width="60px" src="<?=$this->assetUrl('upload/img/flag_fra.png')?>">
+				</div>
+			</div>
+		</div>
+		<div class="flip-container" ontouchstart="this.classList.toggle('hover');">
+			<div class="flipper">
+				<div class="front">
+					<img height="297px" width="250px" src="<?=$this->assetUrl('upload/img/Paul-IOANID.jpg')?>">
+				</div>
+				<div class="back">
+					nom: Paul Ioanid <br>
+					<img height="38px" width="60px" src="<?=$this->assetUrl('upload/img/flag_rom.png')?>">
+				</div>
+			</div>
+		</div>
+		<div class="flip-container" ontouchstart="this.classList.toggle('hover');">
+			<div class="flipper">
+				<div class="front">
+					<img height="297px" width="250px" src="<?=$this->assetUrl('upload/img/Philippe-TASCH.jpg')?>">
+				</div>
+				<div class="back">
+					nom: Philippe Tasch <br>
+					<img height="38px" width="60px" src="<?=$this->assetUrl('upload/img/flag_lux.png')?>">
+				</div>
+			</div>
+		</div>
 
-		<div>Michel
-		<img height="297px" width="250px" src="<?= $this->assetUrl('upload/img/Michel-CAVRO.jpg') ?>">
-		nom: Michel Cavro
-		linkedin.com/in/michelcavro
-		<img height="38px" width="60px" src="<?=$this->assetUrl('upload/img/flag_fra.png')?>"></div>
-
-		<div>Paul
-		<img height="297px" width="250px" src="<?= $this->assetUrl('upload/img/Paul-IOANID.jpg') ?>">
-		nom: Paul Ioanid
-		<img height="38px" width="60px" src="<?=$this->assetUrl('upload/img/flag_rom.png')?>"></div>
-
-		<div>Philippe
-		<img height="297px" width="250px" src="<?= $this->assetUrl('upload/img/Philippe-TASCH.jpg') ?>">
-		nom: Philippe Tasch
-		<img height="38px" width="60px" src="<?=$this->assetUrl('upload/img/flag_lux.png')?>"></div>
 		<?php $this->stop('main_content') ?>
--- app/templates/user/register.php
+++ app/templates/user/register.php
@@ -3,38 +3,47 @@
 <?php $this->start('main_content') ?>
 	<h2>Inscription</h2>
 	<h4>Bonjour, entrez les informations qui vous ont été transmises par mail</h4>
-	<form method="post" action="" enctype="multipart/form-data">
-		* Email
-		<input type="email" name="email" value="">
-		
-		* mot de passe
-		<input type="password" name="password" value="">
-		
-		Rue
-		<input type="text" name="street" value="">
-		
-		Ville
-		<input type="text" name="city" value="">
-		
-		Code postal
-		<input type="text" name="zipcode" value="">
-		
-		Pays
-		<input type="text" name="country" value="">
-		
-		Date de naissance
-		<input type="date" name="birthdate" value="">
-		
-		
-		<img height="90px" width="90px" src="<?= $this->assetUrl('upload/img/avatar_0.png') ?>">
 
-		<input type="hidden" name="fichierSoumis" value="1">
-		<label for="files">Ajoutez votre photo (fichiers autorisés: jpg, jpeg, gif, png, maximum 350Ko)</label>
-		<input style="visibility: hidden;" id = "files" type="file" name="photo">
+<div class="container">
+    <form>
+      <div class="form-group">
+        <label for="Email1">* Adresse Email</label>
+        <input type="email" class="form-control" id="InputEmail1" placeholder="Email" name="email" value="">
+      </div>
+      <div class="form-group">
+        <label for="Password">* Mot de passe</label>
+        <input type="password" class="form-control" id="InputPassword" placeholder="Password" name="password" value="">
+      </div>
+      <div class="form-group">
+        <label for="Street">Rue</label>
+        <input type="text" class="form-control" id="InputStreet" placeholder="Rue" name="street" value="">
+      </div>
+      <div class="form-group">
+        <label for="City">Rue</label>
+        <input type="text" class="form-control" id="InputCity" placeholder="Ville" name="city" value="">
+      </div>
+      <div class="form-group">
+        <label for="ZipCode">Code postal</label>
+        <input type="text" class="form-control" id="InputZipCode" placeholder="Code postal" name="zipcode" value="">
+      </div>
+      <div class="form-group">
+        <label for="Country">Ville</label>
+        <input type="text" class="form-control" id="InputCountry" placeholder="Pays" name="country" value="">
+      </div>
+      <div class="form-group">
+        <label for="Birthdate">Date de naissance</label>
+        <input type="date" class="form-control" id="InputBirthdate" placeholder="Date de naissance" name="birthdate" value="">
+      </div>
+      <div class="form-group">
+      	<img height="90px" width="90px" src="<?= $this->assetUrl('upload/img/avatar_0.png') ?>">
+        <label for="InputFile">Changez la photo</label>
+        <input type="file" id="exampleInputFile" name="street" value="">
+        <p class="help-block">(fichiers autorisés: jpg, jpeg, gif, png, maximum 350Ko)</p>
+      </div>
+      <div>* Champs obligatoires</div>
+      <button type="submit" class="btn btn-default">Valider</button>
+    </form>
+  </div>
 
-		<div>* Champs obligatoires</div>
-		
-		<input type="submit" value="Valider">
-	</form>
 <?php $this->stop('main_content') ?>
 
