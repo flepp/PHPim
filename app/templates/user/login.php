2a7b4013dc7c1f123817e4a0621de19fc8f9d181
Michel Cavro <mcavro@pt.lu>
2016-07-26 13:37:52 +0200

    WIP styles sur "Login" et "Register"

--- app/templates/user/login.php
+++ app/templates/user/login.php
@@ -1,17 +1,29 @@
 <?php $this->layout('layout', ['title' => 'Login']) ?>
 
 <?php $this->start('main_content') ?>
-	<h2>Connexion</h2>
-	<h4>Bonjour, connectez vous avec le pseudo ou l'adresse email que vous avez fourni à l'inscription et le mot de passe qui vous ont été transmis par mail</h4>
-	<form method="post">
-		Pseudo ou email
-		<input type="text" name="userPseudoOrEmail" value="">
 
-		Mot de passe
-		<input type="password" name="password" value="">
+<div class="container" class="fondNoir">
+	<div class="row">
+		<div class="col">
+			<h1>Connexion</h1>
+			<h2>Bonjour, connectez vous avec le pseudo ou l'adresse email que vous avez fourni à l'inscription et le mot de passe qui vous ont été transmis par mail</h2>
+		</div>
+	    <form>
+	    	<div class="form-group col-md-5">
+	    		<label for="Email">* Pseudo ou email</label>
+	    		<input type="text" class="form-control" placeholder="Email" name="userPseudoOrEmail" value="">
+	    	</div>
+	    	<div class="form-group col-md-5 col-md-offset-2">
+	        	<label for="Password">* Mot de passe</label>
+	        	<input type="password" class="form-control" placeholder="Password" name="password" value="">
+	    	</div>
+	    	<div>* Champs obligatoires</div>
+	    	<button type="submit" class="btn btn-default">Valider</button>
+				<a href="<?= $this->url('user_register') ?>"><button class="btn btn-default">Inscription</button></a>
+				<a href="<?= $this->url('user_forgot')?>"><button class="btn btn-default">Mot de passe oublié?</button></a>
+				<?php $this->stop('main_content') ?>
 
-		<input type="submit" value="Valider">
-	</form>
-<a href="<?= $this->url('user_register') ?>">Inscription</a>
-<a href="<?= $this->url('user_forgot')?>">Mot de passe oublié ?</a>
-<?php $this->stop('main_content') ?>
+	    </form>
+    </div>
+</div>
+
--- app/templates/user/register.php
+++ app/templates/user/register.php
@@ -1,48 +1,52 @@
 <?php $this->layout('layout', ['title' => 'Register']) ?>
 
 <?php $this->start('main_content') ?>
-	<h2>Inscription</h2>
-	<h4>Bonjour, entrez les informations qui vous ont été transmises par mail</h4>
 
-<div class="container">
-    <form>
-      <div class="form-group">
-        <label for="Email1">* Adresse Email</label>
-        <input type="email" class="form-control" id="InputEmail1" placeholder="Email" name="email" value="">
-      </div>
-      <div class="form-group">
-        <label for="Password">* Mot de passe</label>
-        <input type="password" class="form-control" id="InputPassword" placeholder="Password" name="password" value="">
-      </div>
-      <div class="form-group">
-        <label for="Street">Rue</label>
-        <input type="text" class="form-control" id="InputStreet" placeholder="Rue" name="street" value="">
-      </div>
-      <div class="form-group">
-        <label for="City">Rue</label>
-        <input type="text" class="form-control" id="InputCity" placeholder="Ville" name="city" value="">
-      </div>
-      <div class="form-group">
-        <label for="ZipCode">Code postal</label>
-        <input type="text" class="form-control" id="InputZipCode" placeholder="Code postal" name="zipcode" value="">
-      </div>
-      <div class="form-group">
-        <label for="Country">Ville</label>
-        <input type="text" class="form-control" id="InputCountry" placeholder="Pays" name="country" value="">
-      </div>
-      <div class="form-group">
-        <label for="Birthdate">Date de naissance</label>
-        <input type="date" class="form-control" id="InputBirthdate" placeholder="Date de naissance" name="birthdate" value="">
-      </div>
-      <div class="form-group">
-      	<img height="90px" width="90px" src="<?= $this->assetUrl('upload/img/avatar_0.png') ?>">
-        <label for="InputFile">Changez la photo</label>
-        <input type="file" id="exampleInputFile" name="street" value="">
-        <p class="help-block">(fichiers autorisés: jpg, jpeg, gif, png, maximum 350Ko)</p>
-      </div>
-      <div>* Champs obligatoires</div>
-      <button type="submit" class="btn btn-default">Valider</button>
-    </form>
+<div class="container" class="fondNoir">
+	<div class="row">
+		<div class="col">
+			<h1>Inscription</h1>
+			<h2>Bonjour, entrez les informations qui vous ont été transmises par mail</h2>
+		</div>
+	    <form>
+	      <div class="form-group col-md-5">
+	        <label for="Email">* Adresse Email</label>
+	        <input type="email" class="form-control" placeholder="Email" name="email" value="">
+	      </div>
+	      <div class="form-group col-md-5 col-md-offset-2">
+	        <label for="Password">* Mot de passe</label>
+	        <input type="password" class="form-control" placeholder="Password" name="password" value="">
+	      </div>
+	      <div class="form-group col-md-12">
+	        <label for="Street">Rue</label>
+	        <input type="text" class="form-control" placeholder="Rue" name="street" value="">
+	      </div>
+	      <div class="form-group col-md-5">
+	        <label for="City">Ville</label>
+	        <input type="text" class="form-control" placeholder="Ville" name="city" value="">
+	      </div>
+	      <div class="form-group col-md-2 col-md-offset-1">
+	        <label for="ZipCode">Code postal</label>
+	        <input type="text" class="form-control" placeholder="Code postal" name="zipcode" value="">
+	      </div>
+	      <div class="form-group col-md-3 col-md-offset-1">
+	        <label for="Country">Pays</label>
+	        <input type="text" class="form-control" placeholder="Pays" name="country" value="">
+	      </div>
+	      <div class="form-group col-md-3">
+	        <label for="Birthdate">Date de naissance</label>
+	        <input type="date" class="form-control" placeholder="Date de naissance" name="birthdate" value="">
+	      </div>
+	      <div class="form-group col-md-12">
+	      	<img height="90px" width="90px" src="<?= $this->assetUrl('upload/img/avatar_0.png') ?>">
+	        <label for="InputFile">Changez la photo</label>
+	        <input type="file" name="photo" value="">
+	        <p>(fichiers autorisés: jpg, jpeg, gif, png, maximum 350Ko)</p>
+	      <div>* Champs obligatoires</div>
+	      <button type="submit" class="btn btn-default">Valider</button>
+	      </div>
+	    </form>
+    </div>
   </div>
 
 <?php $this->stop('main_content') ?>
