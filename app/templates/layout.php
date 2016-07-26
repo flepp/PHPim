7ce8e7c8ed9bcf791462b3e7127bbf2e02fa19b7
flepp <flepptasch@hotmail.com>
2016-07-26 10:45:44 +0200

    added css and js files + bootstrap

--- app/templates/layout.php
+++ app/templates/layout.php
@@ -5,6 +5,16 @@
 	<title><?= $this->e($title) ?></title>
 	<link rel="stylesheet" href="<?= $this->assetUrl('css/reset.css') ?>">
 	<link rel="stylesheet" href="<?= $this->assetUrl('css/styles.css') ?>">
+	<link rel="stylesheet" href="<?= $this->assetUrl('css/bootstrap.css') ?>">
+	<link rel="stylesheet" href="<?= $this->assetUrl('css/bootstrap-theme.css') ?>">
+
+	<!-- CSS personnel -->
+
+	<link rel="stylesheet" href="<?= $this->assetUrl('css/michel.css') ?>">
+	<link rel="stylesheet" href="<?= $this->assetUrl('css/paul.css') ?>">
+	<link rel="stylesheet" href="<?= $this->assetUrl('css/ibrahima.css') ?>">
+	<link rel="stylesheet" href="<?= $this->assetUrl('css/philippe.css') ?>">
+
 </head>
 <body>
 	<div class="container">
@@ -45,5 +55,6 @@
 	</div>
 	<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.1.0/jquery.min.js"></script>
 	<script src="<?= $this->assetUrl('js/script.js') ?>"></script>
+	<script src="<?= $this->assetUrl('js/bootstrap.js') ?>"></script>
 </body>
 </html>
