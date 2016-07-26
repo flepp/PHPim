41462e71c507fcdbb9e536a58e705affee97737b
pageioa <paul.ioanid@yahoo.com>
2016-07-26 16:46:22 +0200

    Work in progress for frontend.

--- app/templates/allusers/allUsers.php
+++ app/templates/allusers/allUsers.php
@@ -2,11 +2,12 @@
 
 <?php $this->start('main_content') ?>
 
-	<!-- ~~~~~ I'm inserting a "form", for selecting the session, which will be displayed only for "admin" statute ~~~ -->
-	<?php if (isset($sessionList) && sizeof($sessionList) > 0): ?>	
-		<?php if ($_SESSION['user']['usr_role'] == 'admin'): ?>
-		<div class="container">
-		<h1>Choisissez une session:</h1>
+<!-- ~~~~~ I'm inserting a "form", for selecting the session, which will be displayed only for "admin" statute ~~~ -->
+<?php if (isset($sessionList) && sizeof($sessionList) > 0): ?>	
+	<?php if ($_SESSION['user']['usr_role'] == 'admin'): ?>
+	<div class="container container-paul">
+		<div class="section choose-session">
+			<h1>Choisissez une session:</h1>
 			<form action="" method="">
 				<select name="session">
 				<?php foreach ($sessionList as $key => $value) : ?>
@@ -17,27 +18,27 @@
 				<input class="btn btn-default" type="submit" value="Ok"/>
 			</form>
 		</div>
-		<?php endif ?>
 	<?php endif ?>
+<?php endif ?>
 
-	<!-- ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ Here starts the part for "user" statute ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ -->
-	<div class="container">
-		<?php if(count($allUsersTable) > 0) : ?>
-			<?php if ($_SESSION['user']['usr_role'] == 'user'): ?>
-				<h1>Liste des étudiants de ma session:</h1>
-			<?php else: ?>
-				<h1>Les étudiants de <?= $allUsersTable[0]['ses_name']  ?> </h1>
-			<?php endif ?>
-			<div class="table">
-				<div class="row users-display">
-				<?php foreach ($allUsersTable as $key => $value): ?>
-					<div class="col-xs-2"><img height="178px" width="150px" src="<?= $this->assetUrl('upload/img/'.$value['usr_photo']) ?>" alt="">
-						<a href="<?= $this->url('allusers_details', ['id' => $value['id']]) ?>"><?= ' '.$value['usr_firstname'].' '.$value['usr_name'] ?></a>
+		<!-- ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ Here starts the part for "user" statute ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ -->
+		<div class="section list-session">
+			<?php if(count($allUsersTable) > 0) : ?>
+				<?php if ($_SESSION['user']['usr_role'] == 'user'): ?>
+					<h1>Liste des étudiants de ma session:</h1>
+				<?php else: ?>
+					<h1>Les étudiants de <?= $allUsersTable[0]['ses_name']  ?> </h1>
+				<?php endif ?>
+				<div class="table">
+					<div class="row users-display">
+					<?php foreach ($allUsersTable as $key => $value): ?>
+						<div class="col-xs-2"><img height="178px" width="150px" src="<?= $this->assetUrl('upload/img/'.$value['usr_photo']) ?>" alt="">
+							<a href="<?= $this->url('allusers_details', ['id' => $value['id']]) ?>"><?= ' '.$value['usr_firstname'].' '.$value['usr_name'] ?></a>
+						</div>
+					<?php endforeach ?>
 					</div>
-				<?php endforeach ?>
 				</div>
-			</div>
-		<?php endif ?>
+			<?php endif ?>
+		</div>
 	</div>
-
 <?php $this->stop('main_content') ?>
--- app/templates/allusers/details.php
+++ app/templates/allusers/details.php
@@ -3,44 +3,47 @@
 <?php $this->start('main_content') ?>
 
 	<!-- ~~~~~~~~~~~~~~~~~ I'm displaying the specific info for an user ~~~~~~~~~~~~~ -->		
-	<div class="container">
-	<h1>Info:</h1>
-	<img height="297px" width="250px" src="<?= $this->assetUrl('upload/img/'.$userInfo['usr_photo']) ?>">
-	<ul>
-		<li>Nom:<?= ' '.$userInfo['usr_name'] ?></li>
-		<li>Prénom:<?= ' '.$userInfo['usr_firstname'] ?></li>
-		<li>Email:<?= ' '.$userInfo['usr_email'] ?></li>
-		<li>Pseudo:<?= ' '.$userInfo['usr_pseudo'] ?></li>
-		<li>Date de naissance:<?= ' '.$userInfo['usr_birthdate'] ?></li>
-		<li>Ville:<?= ' '.$userInfo['usr_city'] ?></li>
-		<li>Pays:<?= ' '.$userInfo['usr_country'] ?></li>
-		<li>Adresse:<?= ' '.$userInfo['usr_street'] ?></li>
-		<li>Code postal:<?= ' '.$userInfo['usr_zipcode'] ?></li>
-	</ul>
-	
-	<input id="address" type="textbox" value="<?= $userInfo['usr_city'].', '.$userInfo['usr_street'] ?>">
-    
-    <div id="map"></div>
-	
-	<!-- ~~~~~~~~~~~~~~~~~~~~~~ I'm redirecting to all users page ~~~~~~~~~~~~~~~~~~~ -->
-	<a href="<?= $this->url('allusers_allUsers') ?>">Retour vers liste</a>
-	<!-- I'm redirecting to edit user page -->
-	<?php if ($userInfo['id'] == $_SESSION['user']['id']): ?>
-		<a href="<?= $this->url('user_edit', ['id' => $userInfo['id']]) ?>">Editer</a>
-	<?php endif ?>
+	<div class="container-paul">
+		<div class="row user-details">
+			<div class="col-xs-6">
+				<h1>Info:</h1>
+				<img height="297px" width="250px" src="<?= $this->assetUrl('upload/img/'.$userInfo['usr_photo']) ?>">
+				<ul>
+					<li>Nom:<?= ' '.$userInfo['usr_name'] ?></li>
+					<li>Prénom:<?= ' '.$userInfo['usr_firstname'] ?></li>
+					<li>Email:<?= ' '.$userInfo['usr_email'] ?></li>
+					<li>Pseudo:<?= ' '.$userInfo['usr_pseudo'] ?></li>
+					<li>Date de naissance:<?= ' '.$userInfo['usr_birthdate'] ?></li>
+					<li>Ville:<?= ' '.$userInfo['usr_city'] ?></li>
+					<li>Pays:<?= ' '.$userInfo['usr_country'] ?></li>
+					<li>Adresse:<?= ' '.$userInfo['usr_street'] ?></li>
+					<li>Code postal:<?= ' '.$userInfo['usr_zipcode'] ?></li>
+				</ul>
+			</div>
+			<div class="col-xs-6">
+				<input hidden id="address" type="textbox" value="<?= $userInfo['usr_city'].', '.$userInfo['usr_street'] ?>">
+			    
+			    <div id="map"></div>
+				<!-- ~~~~~~~~~~~~~~~~~~~~~~ I'm redirecting to all users page ~~~~~~~~~~~~~~~~~~~ -->
+				<a href="<?= $this->url('allusers_allUsers') ?>">Retour vers liste</a>
+				<!-- I'm redirecting to edit user page -->
+				<?php if ($userInfo['id'] == $_SESSION['user']['id']): ?>
+					<a href="<?= $this->url('user_edit', ['id' => $userInfo['id']]) ?>">Editer</a>
+				<?php endif ?>
 
-	<!-- ~~~~~~~~~~~~~~~~~ I'm changing the user profile for admin ~~~~~~~~~~~~~~~~~~ -->
-	<?php if ($_SESSION['user']['usr_role'] == 'admin'): ?> 
-		<h1>Vous êtes connecté en tant que:</h1> 
-		<form method="post" action="">
-			<select name="userInfo">
-				<?php foreach ($usersList as $key => $info): ?>
-					<option value="<?= $info['id'] ?>" "<?= $info['id'] == $_SESSION['user']['id'] ? ' selected="selected"' : '' ?>"><?= $info['usr_name'].' ' ?><?= $info['usr_firstname'] ?></option>
-				<?php endforeach ?>
-			</select>
-			<button type="sumit" name="troll">Troll me</button>
-		</form> 
-	<?php endif ?>
+				<!-- ~~~~~~~~~~~~~~~~~ I'm changing the user profile for admin ~~~~~~~~~~~~~~~~~~ -->
+				<?php if ($_SESSION['user']['usr_role'] == 'admin'): ?> 
+					<h1>Vous êtes connecté en tant que:</h1> 
+					<form method="post" action="">
+						<select name="userInfo">
+							<?php foreach ($usersList as $key => $info): ?>
+								<option value="<?= $info['id'] ?>" "<?= $info['id'] == $_SESSION['user']['id'] ? ' selected="selected"' : '' ?>"><?= $info['usr_name'].' ' ?><?= $info['usr_firstname'] ?></option>
+							<?php endforeach ?>
+						</select>
+						<button class="btn btn-default" type="sumit" name="troll">Troll me</button>
+					</form> 
+				<?php endif ?>
+			</div>
 
 	<script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyCfSWVubN0FtFttDkHjBWTbwINb-VFNbVc&signed_in=true&callback=initMap"async defer>
 	</script>
