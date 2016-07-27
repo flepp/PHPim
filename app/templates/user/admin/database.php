15f0ae78ff05a5daa17b1046e34e21322a08f326
totossj4 <ibrahima.thiello@hotmail.fr>
2016-07-27 16:49:37 +0200

    some changes

--- app/templates/user/admin/database.php
+++ app/templates/user/admin/database.php
@@ -7,7 +7,7 @@
 	<section class="row">
 		<div class="col-xs-8 col-sm-6 col-md-6">
 			<h1>Choisissez une sesion</h1>
-			<form method="POST" action="">
+			<form method="POST" action="" class="custom-form-gold custom-form">
 				<div class="row">
 					<div class="col-xs-6 col-sm-6 col-md-3">
 						<select name="session" class="form-control">
@@ -18,12 +18,12 @@
 					</div>
 				</div>
 				<div class="row">
-					<div class="col-sm-8 form-group col-md-4">
+					<div class="col-sm-8 col-md-4 form-group custom-form-group ">
 						<label>Suffixe de la database:</label>
 						<input type="text" name="suffixe" class="form-control">
 					</div>
 				</div>
-				<button type="submit" class="btn btn-default">Créer</button>
+				<button type="submit" class="custom-button-blue">Créer</button>
 			</form>
 		</div>
 	</section>
