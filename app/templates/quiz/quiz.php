612609c0b8b32f97f2b8270bd2e0ef767d723e2f
flepp <flepptasch@hotmail.com>
2016-07-28 10:53:32 +0200

    reverted quiz view to last totossj4 commit

--- app/templates/quiz/quiz.php
+++ app/templates/quiz/quiz.php
@@ -1,62 +1,43 @@
 <?php $this->layout('layout', ['title' => 'Quiz']) ?>
 
 <?php $this->start('main_content') ?>
-<div class="container">
-	<h1 class="h1">Quiz</h1>
-	<p>Évaluez vos compétences acquises en cours en répondant aux quiz élaborés par votre formateur.</p>
-	<div class="row">
+<h1>Quiz</h1>
 <?php if (isset($w_user) && is_array($w_user)): ?>
 	<?php if (strtotime($_SESSION['user']['ses_end']) < time()): ?>
 		<?php foreach ($quizList as $key => $val): ?>
-			<?php foreach ($val as $index => $value): ?>
-				<div class="col-xs-12 col-sm-6 col-md-4">
-					<a class="quiz" href="<?= $value['qui_link'] ?>">
-						<p>
-							<span>Jour : <?php $value['qui_day'] ?></span> <span>Cat</span></p>
-						<h3 class="quiz-content">
-						<?= $value['qui_title'] ?>
-						</h3>
-					</a>
-				</div>
-				<!--<div><?php //$value['cat_name'] ?></div>-->
+		<div>
+			<p><?= $key ?></p>
+			<?php foreach ($quizList[$key] as $index => $value): ?>
+				<p><span><?= $value['qui_title'] ?></span> <a href="<?= $value['qui_link'] ?>">
+					<?= $value['qui_link'] ?></a></p>
 			<?php endforeach ?>
+		</div>
 		<?php endforeach ?>
 	<?php else: ?>
 		<?php foreach ($quizList as $key => $val): ?>
+			<div>
+				<p><?= $key ?></p>
 				<?php foreach ($quizList[$key] as $index => $value): ?>
 					<?php if ($value['qui_status'] == 1): ?>
-					<div class="col-xs-12 col-sm-6 col-md-4">
-						<a class="quiz" href="<?= $value['qui_link'] ?>">
-							<p><span>Jour : <?php $value['qui_day'] ?></span> <span>Cat</span></p>
-							<h3 class="quiz-content">
-								<?= $value['qui_title'] ?>
-							</h3>
-						</a>
-					</div>
+						<p><span><?= $value['qui_title'] ?></span> <a href="<?= $value['qui_link'] ?>">
+						<?= $value['qui_link'] ?></a></p>
 					<?php endif ?>
 				<?php endforeach ?>
+			</div>
 		<?php endforeach ?>
 	<?php endif ?>
 <?php else: ?>
 	<?php foreach ($quizList as $key => $val): ?>
+		<div>
+			<p><?= $key ?></p>
 			<?php foreach ($quizList[$key] as $index => $value): ?>
 				<?php if ($value['qui_status'] == 1): ?>
-				<div class="col-xs-12 col-sm-6 col-md-4">
-					<a class="quiz" href="<?= $value['qui_link'] ?>">
-						<p>
-							<span>Jour : <?php $value['qui_day'] ?></span>
-							<span>Cat</span>
-						</p>
-						<h3 class="quiz-content">
-							<?= $value['qui_title'] ?>
-						</h3>
-					</a>
-				</div>
+					<p><span><?= $value['qui_title'] ?></span> <a href="<?= $value['qui_link'] ?>">
+					<?= $value['qui_link'] ?></a></p>
 				<?php endif ?>
 			<?php endforeach ?>
+		</div>
 	<?php endforeach ?>
 <?php endif ?>
-	</div>
-</div>
 <?php $this->stop('main_content') ?>
 
